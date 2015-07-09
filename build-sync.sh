9e2d5783bbdc1f50d0a2f204fd58e84d75eeecc8
Silas S. Brown <ssb22@cam.ac.uk>
2015-07-09 17:39:49 +0000

    Update WebCheck,build-sync scripts,css-generate.py
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/ohi@2270 29193198-4895-4776-b068-10539e920549

--- build-sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# sync OHI to SVN
-wget -N http://people.ds.cam.ac.uk/ssb22/gradint/ohi.py
-wget -N http://people.ds.cam.ac.uk/ssb22/gradint/ohi_online.py
-wget -N http://people.ds.cam.ac.uk/ssb22/gradint/ohi_latex.py
-svn commit -m "Update OHI"
