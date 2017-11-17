016f5fbd05e418d10665df1e3477bb17f526bc66
Silas S. Brown <ssb22@cam.ac.uk>
2017-11-17 08:42:58 -0600

    update

---
+++ build-sync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -N http://people.ds.cam.ac.uk/ssb22/gradint/ohi.py
+wget -N http://people.ds.cam.ac.uk/ssb22/gradint/ohi_online.py
+wget -N http://people.ds.cam.ac.uk/ssb22/gradint/ohi_latex.py
+git commit -am update && git push
