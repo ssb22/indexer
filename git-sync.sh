a1b54902bdae0fd95f073b80c7b1ff0ef6a15a12
Silas S. Brown <ssb22@cam.ac.uk>
2017-11-26 03:01:20 -0600

    Update git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -2,4 +2,4 @@
 wget -N http://people.ds.cam.ac.uk/ssb22/gradint/ohi.py
 wget -N http://people.ds.cam.ac.uk/ssb22/gradint/ohi_online.py
 wget -N http://people.ds.cam.ac.uk/ssb22/gradint/ohi_latex.py
-git commit -am update && git push
+git commit -am "Update $(echo $(git diff|grep '^--- a/'|sed -e 's,^--- a/,,')|sed -e 's/ /, /g')" && git push
