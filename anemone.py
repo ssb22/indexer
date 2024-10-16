1318c6e0836eb2d4ab7a11a8bf6d559b1542a14b
Silas S. Brown <ssb22@cam.ac.uk>
2024-10-16 12:58:11 +0100

    Anemone clean up multithreading

--- anemone.py
+++ anemone.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Anemone 1.77 (http://ssb22.user.srcf.net/anemone)
+Anemone 1.78 (http://ssb22.user.srcf.net/anemone)
 (c) 2023-24 Silas S. Brown.  License: Apache 2
 
 To use this module, either run it from the command
@@ -148,6 +148,18 @@ re-code the MP3 files to ensure they are constant
 bitrate and more likely to work with the more
 limited DAISY-reading programs like FSReader 3
 (this requires LAME or miniaudio/lameenc)""")
+    args.add_argument("--max-threads",
+                      default=0, help="""
+Maximum number of threads to use for MP3 re-coding.
+If set to 0 (default), the number of CPU cores is
+detected and used, and, if called as a module, multiple
+threads calling anemone() share the same pool of
+MP3 re-coding threads.  This is usually most efficient.
+If set to anything other than 0, a local pool of threads
+is used for MP3 re-coding (instead of sharing the pool
+with any other anemone() instances) and it is limited to
+the number of threads you specify (which might also be
+useful if you don't want to use all available CPU cores).""")
     args.add_argument("--allow-jumps",
                       action="store_true",help="""
 Allow jumps in heading levels e.g. h1 to h3 if the
@@ -589,25 +601,25 @@ class Run():
         if not __name__=="__main__":
             R.info(
                 f"Making {R.outputFile}...") # especially if repeatedly called as a module, better print which outputFile we're working on BEFORE the mp3s as well as after
-        global _theThreadPoolExecutor
-        try: _theThreadPoolExecutor
-        except NameError:
-            # (if Anemone is called by a multithreaded client, we
-            # want only one ThreadPoolExecutor across all threads)
-            _theThreadPoolExecutor = ThreadPoolExecutor(
-                max_workers=cpu_count())
-        recordingTasks=[(_theThreadPoolExecutor.submit(
+        if R.max_threads: executor = ThreadPoolExecutor(
+                max_workers=R.max_threads)
+        else: executor = shared_executor
+        recordingTasks=[(executor.submit(
             (recodeMP3 if R.mp3_recode or
              'audio/mp3' not in R.MFile(dat).mime
              else lambda x,r:x),
             dat, R) if dat else None)
                         for dat in R.audioData]
-    else: recordingTasks = None,None
+    else: executor,recordingTasks = None,None
     try: R.write_all0(recordingTexts,headings,recordingTasks)
     except: # unhandled exception: clean up
+        if R.max_threads and executor:
+            try: executor.shutdown(wait=False)
+            except: pass # (can't do it) # noqa: E722
         try: os.remove(R.outputFile) # incomplete
         except: pass # noqa: E722
         raise
+    if R.max_threads and executor: executor.shutdown()
   def write_all0(self,recordingTexts,headings,recordingTasks) -> None:
     "Service method for write_all"
     R = self
@@ -1803,6 +1815,8 @@ textres = """<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE resources
   PUBLIC "-//NISO//DTD resource 2005-1//EN" "http://www.daisy.org/z3986/2005/resource-2005-1.dtd">
 <resources xmlns="http://www.daisy.org/z3986/2005/resource/" version="2005-1"><!-- SKIPPABLE NCX --><scope nsuri="http://www.daisy.org/z3986/2005/ncx/"><nodeSet id="ns001" select="//smilCustomTest[@bookStruct='LINE_NUMBER']"><resource xml:lang="en" id="r001"><text>Row</text></resource></nodeSet><nodeSet id="ns002" select="//smilCustomTest[@bookStruct='NOTE']"><resource xml:lang="en" id="r002"><text>Note</text></resource></nodeSet><nodeSet id="ns003" select="//smilCustomTest[@bookStruct='NOTE_REFERENCE']"><resource xml:lang="en" id="r003"><text>Note reference</text></resource></nodeSet><nodeSet id="ns004" select="//smilCustomTest[@bookStruct='ANNOTATION']"><resource xml:lang="en" id="r004"><text>Annotation</text></resource></nodeSet><nodeSet id="ns005" select="//smilCustomTest[@id='annoref']"><resource xml:lang="en" id="r005"><text>Annotation reference</text></resource></nodeSet><nodeSet id="ns006" select="//smilCustomTest[@bookStruct='PAGE_NUMBER']"><resource xml:lang="en" id="r006"><text>Page</text></resource></nodeSet><nodeSet id="ns007" select="//smilCustomTest[@bookStruct='OPTIONAL_SIDEBAR']"><resource xml:lang="en" id="r007"><text>Optional sidebar</text></resource></nodeSet><nodeSet id="ns008" select="//smilCustomTest[@bookStruct='OPTIONAL_PRODUCER_NOTE']"><resource xml:lang="en" id="r008"><text>Optional producer note</text></resource></nodeSet></scope><!-- ESCAPABLE SMIL --><scope nsuri="http://www.w3.org/2001/SMIL20/"><nodeSet id="esns001" select="//seq[@bookStruct='line']"><resource xml:lang="en" id="esr001"><text>Row</text></resource></nodeSet><nodeSet id="esns002" select="//seq[@class='note']"><resource xml:lang="en" id="esr002"><text>Note</text></resource></nodeSet><nodeSet id="esns003" select="//seq[@class='noteref']"><resource xml:lang="en" id="esr003"><text>Note reference</text></resource></nodeSet><nodeSet id="esns004" select="//seq[@class='annotation']"><resource xml:lang="en" id="esr004"><text>Annotation</text></resource></nodeSet><nodeSet id="esns005" select="//seq[@class='annoref']"><resource xml:lang="en" id="esr005"><text>Annotation reference</text></resource></nodeSet><nodeSet id="esns006" select="//seq[@class='pagenum']"><resource xml:lang="en" id="esr006"><text>Page</text></resource></nodeSet><nodeSet id="esns007" select="//seq[@class='sidebar']"><resource xml:lang="en" id="esr007"><text>Optional sidebar</text></resource></nodeSet><nodeSet id="esns008" select="//seq[@class='prodnote']"><resource xml:lang="en" id="esr008"><text>Optional producer note</text></resource></nodeSet></scope><!-- ESCAPABLE DTBOOK --><scope nsuri="http://www.daisy.org/z3986/2005/dtbook/"><nodeSet id="ns009" select="//annotation"><resource xml:lang="en" id="r009"><text>Annotation</text></resource></nodeSet><nodeSet id="ns010" select="//blockquote"><resource xml:lang="en" id="r010"><text>Quote</text></resource></nodeSet><nodeSet id="ns011" select="//code"><resource xml:lang="en" id="r011"><text>Code</text></resource></nodeSet><nodeSet id="ns012" select="//list"><resource xml:lang="en" id="r012"><text>List</text></resource></nodeSet><nodeSet id="ns018" select="//note"><resource xml:lang="en" id="r018"><text>Note</text></resource></nodeSet><nodeSet id="ns013" select="//poem"><resource xml:lang="en" id="r013"><text>Poem</text></resource></nodeSet><nodeSet id="ns0014" select="//prodnote[@render='optional']"><resource xml:lang="en" id="r014"><text>Optional producer note</text></resource></nodeSet><nodeSet id="ns015" select="//sidebar[@render='optional']"><resource xml:lang="en" id="r015"><text>Optional sidebar</text></resource></nodeSet><nodeSet id="ns016" select="//table"><resource xml:lang="en" id="r016"><text>Table</text></resource></nodeSet><nodeSet id="ns017" select="//tr"><resource xml:lang="en" id="r017"><text>Table row</text></resource></nodeSet></scope></resources>"""
+
+shared_executor = ThreadPoolExecutor(max_workers=cpu_count())
 
 if __name__ == "__main__": anemone()
 
